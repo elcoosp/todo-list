7c7348133ecb82cf6afd4a2efd1130f9c9bece2f
Elcoosp <elcoosp@gmail.com>
2018-07-24 21:19:15 +0200

    fix(Refactor payload enhancers):

--- src/components/TodoList/actions.js
+++ src/components/TodoList/actions.js
@@ -1,17 +1,20 @@
-import { makeAction } from '../../utils'
+import uuid from 'uuid'
+import { makeAction, withFuncForKey } from '../../utils'
 export const UPDATE_TODO = 'UPDATE_TODO'
 export const CREATE_TODO = 'CREATE_TODO'
 export const DELETE_TODO = 'DELETE_TODO'
 export const TOGGLE_TODO_DONE = 'TOGGLE_TODO_DONE'
 
 // Helpers to enhance the payload of actions
-const withDate = key => () => ({
-  [key]: Date.now()
-})
-const withUpdatedAt = withDate('updatedAt')
+const withId = withFuncForKey('id', uuid.v4)
+const withUpdatedAt = withFuncForKey('updatedAt', Date.now)
 
 export const actionCreators = {
   toggleTodoDone: makeAction(TOGGLE_TODO_DONE, withUpdatedAt),
   updateTodo: makeAction(UPDATE_TODO, withUpdatedAt),
-  createTodo: makeAction(CREATE_TODO, withDate('createdAt'))
+  createTodo: makeAction(
+    CREATE_TODO,
+    withFuncForKey('createdAt', Date.now),
+    withId
+  )
 }
--- src/utils.js
+++ src/utils.js
@@ -3,10 +3,14 @@ import React from 'react'
 export const mapToComp = (data, Componnent, key = 'id') =>
   data.map(props => <Componnent key={props[key]} {...props} />)
 
-export const makeAction = (type, payloadEnhancer) => payload => ({
+export const makeAction = (type, ...payloadEnhancers) => payload => ({
   type,
   payload: {
     ...payload,
-    ...payloadEnhancer(payload)
+    ...payloadEnhancers.reduce((o, f) => Object.assign(o, f(payload)), {})
   }
+})
+
+export const withFuncForKey = (key, func) => () => ({
+  [key]: func()
 })
