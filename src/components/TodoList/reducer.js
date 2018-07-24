911e5ab6339235f000287c1c07fb70f120c5563a
Elcoosp <elcoosp@gmail.com>
2018-07-24 21:17:52 +0200

    feat(TodoList): Add CREATE_TODO to the reducer

--- src/components/TodoList/reducer.js
+++ src/components/TodoList/reducer.js
@@ -1,33 +1,20 @@
-import { over, lensPath, merge } from 'ramda'
+import * as R from 'ramda'
 import * as A from './actions'
-const initialState = {
-  '46df4gdf': {
-    done: true,
-    title: 'Sortir le chien',
-    id: '46df4gdf',
-    body: 'dgjdfkgdfgdgdf',
-    tags: ['gfgd', 'gfdgdgd']
-  },
-  gergeg: {
-    done: false,
-    title: 'Sortir le chigegegen',
-    id: 'gergeg',
-    body: 'dgjdfkgdfgdgdf',
-    tags: ['gfgd', 'gfdgdgd']
-  }
-}
-const todosReducer = (state = initialState, { type, payload }) => {
+
+const todosReducer = (state = {}, { type, payload }) => {
   switch (type) {
     case A.TOGGLE_TODO_DONE:
-      return over(
-        lensPath([payload.id]),
+      return R.over(
+        R.lensProp(payload.id),
         todo => ({ ...todo, ...payload, done: !todo.done }),
         state
       )
     case A.UPDATE_TODO:
-      return over(lensPath([payload.id]), merge(payload), state)
+      return R.over(R.lensProp(payload.id), R.merge(R.__, payload), state)
     case A.CREATE_TODO:
-      return
+      console.log(R.merge(state, { [payload.id]: payload }))
+
+      return R.merge(state, { [payload.id]: payload })
     case A.DELETE_TODO:
       return
     default:
