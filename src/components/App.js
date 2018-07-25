57fa5253ee2f3b80a247ebe692bf2b46bd297335
Elcoosp <elcoosp@gmail.com>
2018-07-25 12:49:23 +0200

    style(TodoList): Add a confetti maker when the add todo button is clicked

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import { Route } from 'react-router-dom'
+import 'antd/dist/antd.css'
 import Header from './Header'
 import Home from './pages/Home'
 import Todos from './pages/Todos'
--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -5,31 +5,65 @@ import { connect } from 'react-redux'
 import { mapToComp } from '../../utils'
 import TodoItem from './TodoItem'
 import TodoForm from './TodoForm'
+import Button from 'antd/lib/button'
+import Confetti from 'react-dom-confetti'
+import { withState, compose } from 'recompose'
 
-const AddTodo = () => <Link to="/todos/add"> Add a wonderful todo</Link>
-const TodoList = ({ todos, match: { url } }) => {
-  return (
-    <section>
-      <Switch>
-        <Route path={`${url}/add`} component={TodoForm} />
-        <Route component={AddTodo} />
-      </Switch>
-      {mapToComp(todos, TodoItem)}
-    </section>
-  )
+const AddTodoButton = ({ onClick }) => (
+  <Button onClick={onClick}>
+    <Link to="/todos/add"> Add a wonderful todo</Link>
+  </Button>
+)
+AddTodoButton.propTypes = {
+  onClick: P.func.isRequired
 }
 
+const TodoList = ({
+  todos,
+  match: { url },
+  isMakingConfetti,
+  shouldMakeConfetti
+}) => (
+  <section>
+    <Switch>
+      <Route path={`${url}/add`} component={TodoForm} />
+      <Route
+        render={() => (
+          <AddTodoButton onClick={() => shouldMakeConfetti(true)} />
+        )}
+      />
+    </Switch>
+    <Confetti
+      active={isMakingConfetti}
+      config={{
+        angle: 90,
+        spread: 234,
+        startVelocity: 75,
+        elementCount: 97,
+        decay: 0.55
+      }}
+    />
+    {mapToComp(todos, TodoItem)}
+  </section>
+)
+
 TodoList.propTypes = {
+  shouldMakeConfetti: P.func.isRequired,
+  isMakingConfetti: P.bool,
   todos: P.arrayOf(P.shape(TodoItem.propTypes)).isRequired,
   match: P.shape({
     url: P.string
   })
 }
 
-export default withRouter(
+const enhance = compose(
+  withState('isMakingConfetti', 'shouldMakeConfetti', false),
+  withRouter,
   connect(state => ({
     todos: Object.values(state.todos).sort(
       (a, b) => (a.createdAt > b.createdAt ? 0 : 1)
     )
-  }))(TodoList)
+  }))
 )
+
+export default enhance(TodoList)
